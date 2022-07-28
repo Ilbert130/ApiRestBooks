d80b0995e94df148937aec15826df8e3e75a8ff7
Ilbert130 <ilbertca27@gmail.com>
2022-07-29 03:59:27 +0800

    Added model Book

--- ApiRestBooks/Startup.cs
+++ ApiRestBooks/Startup.cs
@@ -1,6 +1,46 @@
-﻿namespace ApiRestBooks
+﻿using System.Text.Json.Serialization;
+
+namespace ApiRestBooks
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(Startup));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
